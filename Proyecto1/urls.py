dacf0964eeaf9a0e534b9ff6293175612274cd38
Sergio <43445037+sergioandresco@users.noreply.github.com>
2023-01-31 15:59:09 -0500

    creacion y prueba de vistas

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto1.views import saludo, suma
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
+    path('suma/', suma),
 ]
--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse;
 
+#Creamos vistas para poder utilizarlas
+
 def saludo(request): #primera vista
     
     return HttpResponse("Hola mundo, esta es la primera pagina con Django");
+
+def suma(request):
+    
+    x = 2;
+    y = 2;
+    
+    suma = x + y;
+    
+    return HttpResponse(suma);
